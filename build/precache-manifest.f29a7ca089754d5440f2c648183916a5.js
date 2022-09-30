5da0bc534f99d6ad4782f2a55f14b6cae6e02d76
jsdawn <kingwyh1993@163.com>
2022-09-30 16:25:05 +0800

    build

---
+++ build/precache-manifest.f29a7ca089754d5440f2c648183916a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dbcc4869b40d59a9912",
+    "url": "./static/css/main.0a1b4288.chunk.css"
+  },
+  {
+    "revision": "7dbcc4869b40d59a9912",
+    "url": "./static/js/main.7dbcc486.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "35a3f7863c8ca12375ca",
+    "url": "./static/css/2.837d39fb.chunk.css"
+  },
+  {
+    "revision": "35a3f7863c8ca12375ca",
+    "url": "./static/js/2.35a3f786.chunk.js"
+  },
+  {
+    "revision": "dd2689709516ac5893d1c5a178a1f172",
+    "url": "./static/media/qrcode.dd268970.jpeg"
+  },
+  {
+    "revision": "b1e36729a0f708435164f00415b3eb10",
+    "url": "./index.html"
+  }
+];
